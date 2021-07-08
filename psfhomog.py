86cc549a0df7c46c25dda1dda28a4e04fcbc5029
Jonathan Cohn <jonathancohn@Jonathans-MacBook-Pro-2.local>
2021-07-08 16:15:21 -0500

    update

--- psfhomog.py
+++ psfhomog.py
@@ -4,6 +4,8 @@ import numpy as np
 from scipy import interpolate, optimize, ndimage
 from photutils.centroids import centroid_com, centroid_2dg  # centroid_quadratic,
 import time
+from astropy import convolution
+
 
 def gaussian(height, center_x, center_y, width_x, width_y):
     """Returns a gaussian function with the given parameters"""
@@ -32,6 +34,34 @@ def fitgaussian(data):
                                  data)
     p, success = optimize.leastsq(errorfunction, params)
     return p
+
+
+def fitconvolve(sigma, data1, data2):
+    """
+
+    :return:
+    """
+
+    if sigma[0] <= 1e-15 or np.isinf(sigma[0]) or sigma[1] <= 1e-15 or np.isinf(sigma[1]):
+        zero = np.inf
+
+    else:
+        # convolve I-band PSF (data1) with kernel with unknown FWHM (sigma[0], sigma[1]) to match H-band PSF (data2)
+        zero = np.nansum(ndimage.gaussian_filter(data1, [sigma[0], sigma[1]]) - data2)
+
+    return [zero, 0.]
+
+def fitsigma(sigma, data1, data2):
+
+    return np.nansum(ndimage.gaussian_filter(data1, [sigma[0], sigma[1]]) - data2)
+
+def vfit(sigma, *kwargs):
+
+    return [fitsigma(sigma, *kwargs), 0.]
+
+def twoD_gaussian(x, height, center_x, center_y, width_x, width_y):
+    """Returns a gaussian function with the given parameters"""
+    return height*np.exp(-(((center_x-x[0])/width_x)**2+((center_y-x[1])/width_y)**2)/2)
 
 def do_2dgaussfit(data, plots=False):
     data = np.nan_to_num(data)
@@ -119,6 +149,138 @@ with fits.open(p11179_f814w_pxf08) as hdu:
 
 res = 0.06  # arcsec/pix
 
+# METHOD 2
+t0 = time.time()
+print('start')
+def twoD_gaussian(x, height, center_x, center_y, width_x, width_y):
+    """Returns a gaussian function with the given parameters"""
+    return height*np.exp(-(((center_x-x[0])/width_x)**2+((center_y-x[1])/width_y)**2)/2)
+
+def conv(pars, dataI):
+    height, center_x, center_y, width_x, width_y = pars
+    #height, width_x, width_y = pars
+    #center_x, center_y = 40., 40.
+
+    x = np.linspace(0, len(dataI), len(dataI))
+    y = np.linspace(0, len(dataI[0]), len(dataI[0]))
+    x, y = np.meshgrid(x, y)
+
+    twoDG = np.exp(-(((center_x - x) / width_x) ** 2 + ((center_y - y) / width_y) ** 2) / 2)
+
+    return convolution.convolve(dataI * height, twoDG)
+
+def zero_conv(pars, dataH, dataI):
+
+    icon = conv(pars, dataI)
+
+    # return np.abs(np.nansum(icon - dataH))
+    return np.abs(np.nansum((icon - dataH)**2))
+    # return np.exp(-np.nansum(icon - dataH))
+
+
+# psfH peak is centered on 40,40; current peak is at NGC 1927,1922 -> 1882:1882+81, 1887:1887+81
+# dat_psfi = dat_ngc_psfcen_f814w[1882:1882+81, 1887:1887+81]  # correct!
+dat_psfi = dat_pgc_psfcen_f814w[1606:1606+81, 1542:1542+81]
+# psfH peak is centered on 40,40; current peak is at PGC 1646.4766,1582.24407 -> 1606:1606+81, 1542:1542+81
+#g2di = do_2dgaussfit(dat_psfi)  # [1.04957099e-03 1.46476646e+02 8.22440674e+01 8.65402657e-01 8.51768218e-01] # p11179
+#plt.imshow(dat_psfi, origin='lower')
+#plt.colorbar()
+#plt.show()
+#print(oops)
+g2di = do_2dgaussfit(dat_psfi)  # [1.10704459e-03 4.01447195e+01 4.01799546e+01 8.18216509e-01 8.47028033e-01] # n384
+g2dh = do_2dgaussfit(dat_psfH)  # [ 0.04847133 40.03064685 39.91339118  1.45843277  1.49161195]  # PSF H
+
+# dat_psfi *= g2dh[0] / g2di[0]
+
+
+diff_g2d = np.sqrt(g2dh**2 - g2di**2)
+print(diff_g2d)
+guess_g2d_offset = (160., 40., 40., 1.2, 1.2)
+#guess_g2d_offset = (160., 1.2, 1.2)
+# guess_g2d_best = (159., 40., 40., diff_g2d[3], diff_g2d[4])
+#z = zero_conv(pars=guess_g2d_best, dataH=dat_psfH, dataI=dat_psfi)
+#print(z, 'check')
+#z = zero_conv(pars=guess_g2d_offset, dataH=dat_psfH, dataI=dat_psfi)
+#print(z, 'check')
+sol = optimize.fmin(zero_conv, x0=guess_g2d_offset, args=(dat_psfH, dat_psfi))
+print(sol, 'fmin solution')
+print(diff_g2d)
+# NGC 384:
+# [120.0546269   40.21214241  40.35472934   1.09384867   1.06637347] fmin solution (including centerx,y) 0.0004500262
+# [121.55822138   1.13873817   1.15121924] fmin solution (NOT including centerx,y) 0.0010782303
+# PGC 11179:
+# [121.84288026  40.1249686   40.02693034   1.09615983   1.05934121] fmin solution (including centerx,y) 0.00045021565
+# [121.82100697   1.10433544   1.05780877] fmin solution (NOT including centerx,y) 0.0005105181
+
+# MAKE GALAXY PLOTS
+sol[0] = 1.  # comment out for withamp; else, noamp!
+sol[1] = sol[1]+60.  # 200 vs 80 -> midpoint 100 instead of 40, so add 60!
+sol[2] = sol[2]+60.  # 200 vs 80 -> midpoint 100 instead of 40, so add 60!
+# icon = conv(sol, dataI=dat_nI[1820:2021, 1830:2031])  # NGC 384
+icon = conv(sol, dataI=dat_pI[1550:1751, 1500:1701])  # PGC 11179
+
+fig, ax = plt.subplots(1, 2, figsize=(12,5))
+plt.subplots_adjust(wspace=0.05)
+#im0 = ax[0].imshow(dat_nI[1820:2021, 1830:2031], origin='lower', vmax=150.)  # NGC 384
+im0 = ax[0].imshow(dat_pI[1550:1751, 1500:1701], origin='lower', vmax=90.)  # PGC 11179
+cb0 = fig.colorbar(im0, ax=ax[0], pad=0.02)
+ax[0].text(5, 170, 'I-band (original)', color='w')
+# im0 = ax[1].imshow(icon, origin='lower', vmax=150.)  # NGC 384 (noamp)
+# im0 = ax[1].imshow(icon, origin='lower', vmax=12000.)  # NGC 384 (withamp)
+im0 = ax[1].imshow(icon, origin='lower', vmax=90.)  # PGC 11179 (noamp)
+# im0 = ax[1].imshow(icon, origin='lower', vmax=6800.)  # PGC 11179 (withamp)
+cb0 = fig.colorbar(im0, ax=ax[1], pad=0.02)
+ax[1].text(5, 170, 'I-band (convolved)', color='w')
+plt.show()
+print(oops)
+# n384_psfhomog_2dgauss_5pars_iband_noamp.png  # p11179_psfhomog_2dgauss_5pars_iband_noamp
+#
+
+# MAKE PSF PLOTS
+icon = conv(sol, dataI=dat_psfi)
+print(zero_conv(pars=sol, dataH=dat_psfH, dataI=dat_psfi), 'test the solution!')
+
+fig, ax = plt.subplots(2, 2, figsize=(8,8))
+fig.subplots_adjust(wspace=0.1, hspace=0.1)
+im0 = ax[0][0].imshow(dat_psfi, origin='lower')
+ax[0][0].text(5, 70, 'I-band PSF', color='w')
+cbar0 = fig.colorbar(im0, ax=ax[0][0], pad=0.02)
+im01 = ax[0][1].imshow(dat_psfH, origin='lower')
+ax[0][1].text(5, 70, 'H-band PSF', color='w')
+cbar01 = fig.colorbar(im01, ax=ax[0][1], pad=0.02)
+im1 = ax[1][0].imshow(icon, origin='lower')
+ax[1][0].text(5, 70, 'convolved I-band', color='w')
+cbar1 = fig.colorbar(im1, ax=ax[1][0], pad=0.02)
+im2 = ax[1][1].imshow((dat_psfH - icon), origin='lower')
+ax[1][1].text(5, 70, r'H-band $-$ convolved I-band', color='w')
+cbar2 = fig.colorbar(im2, ax=ax[1][1], pad=0.02)
+plt.show()
+print(oop)
+#xx = optimize.fsolve(vfit, args=(dat_psfi, dat_psfH), x0=[143.4, 2])
+#print(xx, vfit(xx, dat_psfi, dat_psfH))
+
+# print(oop)
+print(np.nanmax(dat_psfi), np.nanmax(dat_psfH))  # 0.001129155 0.05456335
+ssp_n = ndimage.gaussian_filter(dat_psfi, [1.2, 1.2]) # convolve with kernel!
+plt.imshow(ssp_n - dat_psfi, origin='lower')
+plt.colorbar()
+plt.show()
+print(np.nansum(dat_psfi - dat_psfH))  # -0.980911
+print(np.nansum(ssp_n - dat_psfi))  # 1.1641532e-10
+print(np.nansum(ssp_n - dat_psfH))  # -0.9809109
+print(oops)
+
+
+print(oops)
+
+solveit = optimize.fsolve(fitconvolve, args=(dat_psfi, dat_psfH), x0=[1.2, 1.2])
+print(solveit, [diff_sigma_nx, diff_sigma_ny], 'solution, initial guess')
+print(time.time()-t0, 'time!')
+print(fitconvolve(solveit, dat_psfi, dat_psfH), 'should be 0 -- did solution work?')
+print(oops)
+
+
+# METHOD 1
 # calculate sigmas from 2D gaussians
 gp = do_2dgaussfit(dat_pgc_psfcen_f814w[1500:1700, 1500:1700])  # [1.04957099e-03 1.64647665e+03 1.58224407e+03 8.65402658e-01 8.51768218e-01]
 gn = do_2dgaussfit(dat_ngc_psfcen_f814w[1820:2020, 1800:2020])  # [1.10704459e-03 1.92214472e+03 1.92717995e+03 8.18216509e-01 8.47028032e-01]
@@ -136,23 +298,34 @@ print(diff_sigma_nx)
 print(diff_sigma_ny)
 print(diff_sigma_px)
 print(diff_sigma_py)
+
 tn = time.time()
 ssp_n = ndimage.gaussian_filter(dat_ngc_psfcen_f814w, [diff_sigma_nx, diff_sigma_ny]) # convolve with kernel!
-# ssp_n = ndimage.gaussian_filter(dat_nI, [diff_sigma_nx, diff_sigma_ny]) # convolve with kernel!
+gal_n = ndimage.gaussian_filter(dat_nI, [diff_sigma_nx, diff_sigma_ny]) # convolve with kernel!
 t1 = time.time()
 print('time in gaussian filter', t1 - tn)
 ssp_p = ndimage.gaussian_filter(dat_pgc_psfcen_f814w, [diff_sigma_px, diff_sigma_py]) # convolve with kernel!
-# ssp_p = ndimage.gaussian_filter(dat_pI, [diff_sigma_px, diff_sigma_py]) # convolve with kernel!
+gal_p = ndimage.gaussian_filter(dat_pI, [diff_sigma_px, diff_sigma_py]) # convolve with kernel!
 print('time in gaussian filter', time.time() - t1)
 gn2 = do_2dgaussfit(ssp_n[1820:2020, 1800:2020])  # [1.04957099e-03 1.64647665e+03 1.58224407e+03 8.65402658e-01 8.51768218e-01]
 gp2 = do_2dgaussfit(ssp_p[1500:1700, 1500:1700])  # [1.04957099e-03 1.64647665e+03 1.58224407e+03 8.65402658e-01 8.51768218e-01]
-print(gn2)
-print(gp2)
+gn = do_2dgaussfit(dat_ngc_psfcen_f814w[1820:2020, 1800:2020])  # [1.04957099e-03 1.64647665e+03 1.58224407e+03 8.65402658e-01 8.51768218e-01]
+gp = do_2dgaussfit(dat_pgc_psfcen_f814w[1500:1700, 1500:1700])
+# print(gn2)
+# print(gp2)
+# print(gn)
+# print(gp)
 print(gH)
-plt.imshow(ssp_n, origin='lower')
+plt.imshow(gal_n[1820:2020, 1830:2030], origin='lower', vmax=150.)
 plt.colorbar()
 plt.show()
-plt.imshow(ssp_p, origin='lower')
+plt.imshow(dat_nI[1820:2020, 1830:2030], origin='lower', vmax=150.)
+plt.colorbar()
+plt.show()
+plt.imshow(gal_p[1550:1750, 1500:1700], origin='lower', vmax=90.)
+plt.colorbar()
+plt.show()
+plt.imshow(dat_pI[1550:1750, 1500:1700], origin='lower', vmax=90.)
 plt.colorbar()
 plt.show()
 print(oops)
