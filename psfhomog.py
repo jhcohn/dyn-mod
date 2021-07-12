718d36798b87f65e235b0d6df118a40a6c845b01
Jonathan Cohn <jonathancohn@Jonathans-MacBook-Pro-2.local>
2021-07-12 15:16:20 -0500

    updates and corrections

--- psfhomog.py
+++ psfhomog.py
@@ -68,7 +68,7 @@ def do_2dgaussfit(data, plots=False):
     params = fitgaussian(data)
     fit = gaussian(*params)
     (height, x, y, width_x, width_y) = params
-    print(params)
+    # print(params)
 
     if plots:
         plt.contour(fit(*np.indices(data.shape)))
@@ -163,10 +163,7 @@ def twoD_gaussian(x, height, center_x, center_y, width_x, width_y):
     return height*np.exp(-(((center_x-x[0])/width_x)**2+((center_y-x[1])/width_y)**2)/2)
 
 def conv(pars, dataI):
-    #center_x, center_y, width_x, width_y = pars
     height, center_x, center_y, width_x, width_y = pars
-    #height, width_x, width_y = pars
-    #center_x, center_y = 40., 40.
 
     x = np.linspace(0, len(dataI), len(dataI))
     y = np.linspace(0, len(dataI[0]), len(dataI[0]))
@@ -175,15 +172,40 @@ def conv(pars, dataI):
     twoDG = np.exp(-(((center_x - x) / width_x) ** 2 + ((center_y - y) / width_y) ** 2) / 2)
 
     return convolution.convolve(dataI * height, twoDG)
-    #return convolution.convolve(dataI, twoDG)
 
 def zero_conv(pars, dataH, dataI):
 
     icon = conv(pars, dataI)
+    icon /= np.nanmax(icon)
 
-    # return np.abs(np.nansum(icon - dataH))
     return np.abs(np.nansum((icon - dataH)**2))
-    # return np.exp(-np.nansum(icon - dataH))
+
+def conv_noamp(pars, dataI):
+    center_x, center_y, width_x, width_y = pars
+
+    x = np.linspace(0, len(dataI), len(dataI))
+    y = np.linspace(0, len(dataI[0]), len(dataI[0]))
+    x, y = np.meshgrid(x, y)
+
+    twoDG = np.exp(-(((center_x - x) / width_x) ** 2 + ((center_y - y) / width_y) ** 2) / 2)
+
+    return convolution.convolve(dataI, twoDG)
+
+def zero_conv_noamp(pars, dataH, dataI):
+
+    icon = conv_noamp(pars, dataI)
+    icon /= np.nanmax(icon)
+
+    return np.abs(np.nansum((icon - dataH)**2))
+
+def matchfwhm(pars, dath, dati):
+    sigx, sigy = pars
+    g2dh = do_2dgaussfit(dath)
+    convolved = ndimage.gaussian_filter(dati, [sigx, sigy])  # convolve with kernel!
+    g2dicon = do_2dgaussfit(convolved)
+    sigma_diffs = np.array([(g2dicon[3] - g2dh[3])**2, (g2dicon[4] - g2dh[4])**2])
+
+    return np.abs(sum(sigma_diffs))
 
 
 # NORMALIZE PSFs!
@@ -205,6 +227,247 @@ dat_ppsfi = dat_pgc_psfcen_f814w[1606:1606+81, 1542:1542+81]
 #plt.imshow(dat_psfH, origin='lower')
 #plt.colorbar()
 #plt.show()
+#plt.imshow((dat_npsfi - dat_psfH)/dat_psfH, origin='lower')
+#plt.text(5, 70, '(PSF_I - PSF_H)/PSF_H', color='w')
+#plt.colorbar()
+#plt.show()
+#print(oops)
+
+# METHOD 1
+# calculate sigmas from 2D gaussians
+# gn = do_2dgaussfit(dat_ngc_psfcen_f814w[1820:2021, 1800:2021])  # [1.10704459e-03 1.92214472e+03 1.92717995e+03 8.18216509e-01 8.47028032e-01]
+# gp = do_2dgaussfit(dat_pgc_psfcen_f814w[1500:1701, 1500:1701])  # [1.04957099e-03 1.64647665e+03 1.58224407e+03 8.65402658e-01 8.51768218e-01]
+gn = do_2dgaussfit(dat_npsfi)  # [1.10704459e-03 1.92214472e+03 1.92717995e+03 8.18216509e-01 8.47028032e-01]
+gp = do_2dgaussfit(dat_ppsfi)  # [1.04957099e-03 1.64647665e+03 1.58224407e+03 8.65402658e-01 8.51768218e-01]
+gH = do_2dgaussfit(dat_psfH)  # [ 0.04847133 40.03064685 39.91339118  1.45843277  1.49161195]
+
+diff_sigma_px = np.sqrt(gH[3]**2 - gp[3]**2) # diff in quadrature!  # 1.2072894820619162
+diff_sigma_py = np.sqrt(gH[4]**2 - gp[4]**2) # diff in quadrature!  # 1.2277824434215172
+diff_sigma_nx = np.sqrt(gH[3]**2 - gn[3]**2) # diff in quadrature!  # 1.1739269102067171
+diff_sigma_ny = np.sqrt(gH[4]**2 - gn[4]**2) # diff in quadrature!  # 1.2244987215858145
+
+n_psficon_m1 = ndimage.gaussian_filter(dat_npsfi, [diff_sigma_nx, diff_sigma_ny]) # convolve with kernel!
+n_daticon_m1 = ndimage.gaussian_filter(dat_nI, [diff_sigma_nx, diff_sigma_ny]) # convolve with kernel!
+p_psficon_m1 = ndimage.gaussian_filter(dat_ppsfi, [diff_sigma_px, diff_sigma_py]) # convolve with kernel!
+p_daticon_m1 = ndimage.gaussian_filter(dat_pI, [diff_sigma_px, diff_sigma_py]) # convolve with kernel!
+n_psficon_m1 /= np.nanmax(n_psficon_m1)
+p_psficon_m1 /= np.nanmax(p_psficon_m1)
+# PSFs: n_psficon_m1,p_psficon_m1 ;; GALAXY IMAGES: n_daticon_m1,p_daticon_m1
+print('m1 done')
+'''  #
+fig, ax = plt.subplots(2, 2, figsize=(8,8))
+fig.subplots_adjust(wspace=0.1, hspace=0.1)
+fig.suptitle('Method 1')
+im00 = ax[0][0].imshow((n_psficon_m1 - dat_psfH), origin='lower')
+ax[0][0].text(1, 70, 'NGC (PSF_I - PSF_H)', color='w')
+cbar00 = fig.colorbar(im00, ax=ax[0][0], pad=0.02)
+im01 = ax[0][1].imshow((p_psficon_m1 - dat_psfH), origin='lower')
+ax[0][1].text(1, 70, 'PGC (PSF_I - PSF_H)', color='w')
+cbar01 = fig.colorbar(im01, ax=ax[0][1], pad=0.02)
+im10 = ax[1][0].imshow((n_psficon_m1 - dat_psfH)/dat_psfH, origin='lower')
+ax[1][0].text(1, 70, 'NGC (PSF_I - PSF_H)/PSF_H', color='w')
+cbar10 = fig.colorbar(im10, ax=ax[1][0], pad=0.02)
+im11 = ax[1][1].imshow((p_psficon_m1 - dat_psfH)/dat_psfH, origin='lower')
+ax[1][1].text(1, 70, 'PGC (PSF_I - PSF_H)/PSF_H', color='w')
+cbar11 = fig.colorbar(im11, ax=ax[1][1], pad=0.02)
+plt.show()
+#print(oops)
+# '''  #
+
+# METHOD 2A
+sig_guess = (1.2,1.2)
+n_sol_m2a = optimize.fmin(matchfwhm, x0=sig_guess, args=(dat_psfH, dat_npsfi))
+g2dh = do_2dgaussfit(dat_psfH)
+n_psficon_m2a = ndimage.gaussian_filter(dat_npsfi, [n_sol_m2a[0], n_sol_m2a[1]])  # convolve with kernel!
+n_psficon_m2a /= np.nanmax(n_psficon_m2a)  # re-normalize?!
+g2diconn_m2a = do_2dgaussfit(n_psficon_m2a)
+
+p_sol_m2a = optimize.fmin(matchfwhm, x0=sig_guess, args=(dat_psfH, dat_ppsfi))
+p_psficon_m2a = ndimage.gaussian_filter(dat_ppsfi, [p_sol_m2a[0], p_sol_m2a[1]])  # convolve with kernel!
+p_psficon_m2a /= np.nanmax(p_psficon_m2a)  # re-normalize?!
+g2diconp_m2a = do_2dgaussfit(p_psficon_m2a)
+print(n_sol_m2a)
+print(p_sol_m2a)
+print(g2dh)
+print(g2diconn_m2a)
+print(g2diconp_m2a)
+
+n_daticon_m2a = ndimage.gaussian_filter(dat_nI, [n_sol_m2a[0], n_sol_m2a[1]])  # convolve with kernel!
+p_daticon_m2a = ndimage.gaussian_filter(dat_pI, [p_sol_m2a[0], p_sol_m2a[1]])  # convolve with kernel!
+# PSFs: n_psficon_m2a,p_psficon_m2a ;; GALAXY IMAGES: n_daticon_m2a,p_daticon_m2a
+
+#fig, ax = plt.subplots(1, 2, figsize=(8,8))
+#fig.subplots_adjust(wspace=0.1, hspace=0.1)
+#im0 = ax[0].imshow((n_psficon_m2a - dat_psfH)/dat_psfH, origin='lower')  #  / dat_psfH
+#ax[0].text(5, 70, 'NGC (PSF_I - PSF_H)/PSF_H', color='w')  # /PSF_H
+#cbar0 = fig.colorbar(im0, ax=ax[0], pad=0.02)
+#im1 = ax[1].imshow((p_psficon_m2a - dat_psfH)/dat_psfH, origin='lower')  #  / dat_psfH
+#ax[1].text(5, 70, 'PGC (PSF_I - PSF_H)/PSF_H', color='w')  # /PSF_H
+#cbar1 = fig.colorbar(im1, ax=ax[1], pad=0.02)
+#plt.show()
+'''  #
+fig, ax = plt.subplots(2, 2, figsize=(8,8))
+fig.subplots_adjust(wspace=0.1, hspace=0.1)
+fig.suptitle('Method 2a')
+im00 = ax[0][0].imshow((n_psficon_m2a - dat_psfH), origin='lower')
+ax[0][0].text(1, 70, 'NGC (PSF_I - PSF_H)', color='w')
+cbar00 = fig.colorbar(im00, ax=ax[0][0], pad=0.02)
+im01 = ax[0][1].imshow((p_psficon_m2a - dat_psfH), origin='lower')
+ax[0][1].text(1, 70, 'PGC (PSF_I - PSF_H)', color='w')
+cbar01 = fig.colorbar(im01, ax=ax[0][1], pad=0.02)
+im10 = ax[1][0].imshow((n_psficon_m2a - dat_psfH)/dat_psfH, origin='lower')
+ax[1][0].text(1, 70, 'NGC (PSF_I - PSF_H)/PSF_H', color='w')
+cbar10 = fig.colorbar(im10, ax=ax[1][0], pad=0.02)
+im11 = ax[1][1].imshow((p_psficon_m2a - dat_psfH)/dat_psfH, origin='lower')
+ax[1][1].text(1, 70, 'PGC (PSF_I - PSF_H)/PSF_H', color='w')
+cbar11 = fig.colorbar(im11, ax=ax[1][1], pad=0.02)
+plt.show()
+print(oops)
+# '''  #
+print('m2a done')
+
+
+# METHOD 2B - with amplitude
+use_guess = (1., 40., 40., 1.2, 1.2)
+n_sol_m2b = optimize.fmin(zero_conv, x0=use_guess, args=(dat_psfH, dat_npsfi))
+n_psficon_m2b = conv(n_sol_m2b, dat_npsfi)
+n_psficon_m2b /= np.nanmax(n_psficon_m2b)
+#n_daticon_m2b = conv(n_sol_m2b, dat_nI)
+
+p_sol_m2b = optimize.fmin(zero_conv, x0=use_guess, args=(dat_psfH, dat_ppsfi))
+p_psficon_m2b = conv(p_sol_m2b, dat_ppsfi)
+p_psficon_m2b /= np.nanmax(p_psficon_m2b)
+print(n_sol_m2b)
+print(p_sol_m2b)
+print(oop)
+#p_daticon_m2b = conv(p_sol_m2b, dat_pI)
+# PSFs: n_psficon_m2b,p_psficon_m2b ;; GALAXY IMAGES: n_daticon_m2b,p_daticon_m2b
+print('m2b done')
+'''  #
+fig, ax = plt.subplots(2, 2, figsize=(8,8))
+fig.subplots_adjust(wspace=0.1, hspace=0.1)
+fig.suptitle('Method 2b')
+im00 = ax[0][0].imshow((n_psficon_m2b - dat_psfH), origin='lower')
+ax[0][0].text(1, 70, 'NGC (PSF_I - PSF_H)', color='w')
+cbar00 = fig.colorbar(im00, ax=ax[0][0], pad=0.02)
+im01 = ax[0][1].imshow((p_psficon_m2b - dat_psfH), origin='lower')
+ax[0][1].text(1, 70, 'PGC (PSF_I - PSF_H)', color='w')
+cbar01 = fig.colorbar(im01, ax=ax[0][1], pad=0.02)
+im10 = ax[1][0].imshow((n_psficon_m2b - dat_psfH)/dat_psfH, origin='lower')
+ax[1][0].text(1, 70, 'NGC (PSF_I - PSF_H)/PSF_H', color='w')
+cbar10 = fig.colorbar(im10, ax=ax[1][0], pad=0.02)
+im11 = ax[1][1].imshow((p_psficon_m2b - dat_psfH)/dat_psfH, origin='lower')
+ax[1][1].text(1, 70, 'PGC (PSF_I - PSF_H)/PSF_H', color='w')
+cbar11 = fig.colorbar(im11, ax=ax[1][1], pad=0.02)
+plt.show()
+print(oops)
+# '''  #
+
+# METHOD 2B - no amplitude
+guess_noamp = (40., 40., 1.2, 1.2)
+n_sol_m2bnoamp = optimize.fmin(zero_conv_noamp, x0=guess_noamp, args=(dat_psfH, dat_npsfi))
+n_psficon_m2bnoamp = conv_noamp(n_sol_m2bnoamp, dat_npsfi)
+n_psficon_m2bnoamp /= np.nanmax(n_psficon_m2bnoamp)
+#n_daticon_m2bnoamp = conv(n_sol_m2bnoamp, dat_nI)
+
+p_sol_m2bnoamp = optimize.fmin(zero_conv_noamp, x0=guess_noamp, args=(dat_psfH, dat_ppsfi))
+p_psficon_m2bnoamp = conv_noamp(p_sol_m2bnoamp, dat_ppsfi)
+p_psficon_m2bnoamp /= np.nanmax(p_psficon_m2bnoamp)
+#p_daticon_m2bnoamp = conv(p_sol_m2bnoamp, dat_pI)
+# PSFs: n_psficon_m2bnoamp,p_psficon_m2bnoamp ;; GALAXY IMAGES: n_daticon_m2bnoamp,p_daticon_m2bnoamp
+print(n_sol_m2bnoamp)
+print(p_sol_m2bnoamp)
+print('m2bnoamp done')
+print(oops)
+
+print(n_psficon_m1.shape)
+print(n_psficon_m2a.shape)
+print(n_psficon_m2b.shape)
+'''  #
+fig, ax = plt.subplots(2, 2, figsize=(8,8))
+fig.subplots_adjust(wspace=0.1, hspace=0.1)
+fig.suptitle('Method 2b no amplitude')
+im00 = ax[0][0].imshow((n_psficon_m2bnoamp - dat_psfH), origin='lower')
+ax[0][0].text(1, 70, 'NGC (PSF_I - PSF_H)', color='w')
+cbar00 = fig.colorbar(im00, ax=ax[0][0], pad=0.02)
+im01 = ax[0][1].imshow((p_psficon_m2bnoamp - dat_psfH), origin='lower')
+ax[0][1].text(1, 70, 'PGC (PSF_I - PSF_H)', color='w')
+cbar01 = fig.colorbar(im01, ax=ax[0][1], pad=0.02)
+im10 = ax[1][0].imshow((n_psficon_m2bnoamp - dat_psfH)/dat_psfH, origin='lower')
+ax[1][0].text(1, 70, 'NGC (PSF_I - PSF_H)/PSF_H', color='w')
+cbar10 = fig.colorbar(im10, ax=ax[1][0], pad=0.02)
+im11 = ax[1][1].imshow((p_psficon_m2bnoamp - dat_psfH)/dat_psfH, origin='lower')
+ax[1][1].text(1, 70, 'PGC (PSF_I - PSF_H)/PSF_H', color='w')
+cbar11 = fig.colorbar(im11, ax=ax[1][1], pad=0.02)
+plt.show()
+print(oops)
+# '''  #
+
+
+# MAKE PLOTS
+# PSFs: n_psficon_m1,p_psficon_m1 ;; GALAXY IMAGES: n_daticon_m1,p_daticon_m1
+# PSFs: n_psfconvolved_m2a,p_psfconvolved_m2a ;; GALAXY IMAGES: n_dataconvolved_m2a,p_dataconvolved_m2a
+# PSFs: n_psficon_m2b,p_psficon_m2b ;; GALAXY IMAGES: n_daticon_m2b,p_daticon_m2b
+# PSFs: n_psficon_m2bnoamp,p_psficon_m2bnoamp ;; GALAXY IMAGES: n_daticon_m2bnoamp,p_daticon_m2bnoamp
+'''  #
+fig, ax = plt.subplots(2, 3, figsize=(20,7))
+fig.subplots_adjust(wspace=0.1, hspace=0.1)
+im0 = ax[0][0].imshow(dat_npsfi, origin='lower')
+ax[0][0].text(5, 70, 'NGC PSF I-band', color='w')
+cbar0 = fig.colorbar(im0, ax=ax[0][0], pad=0.02)
+
+im01 = ax[0][1].imshow(n_psficon_m1, origin='lower')
+ax[0][1].text(5, 70, 'NGC PSF I-band M1', color='w')
+cbar01 = fig.colorbar(im01, ax=ax[0][1], pad=0.02)
+
+im02 = ax[0][2].imshow(n_psficon_m2a, origin='lower')
+ax[0][2].text(5, 70, 'NGC PSF I-band M2a', color='w')
+cbar02 = fig.colorbar(im02, ax=ax[0][2], pad=0.02)
+
+im10 = ax[1][0].imshow(n_psficon_m2b, origin='lower')
+ax[1][0].text(5, 70, 'NGC PSF I-band M2b', color='w')
+cbar10 = fig.colorbar(im10, ax=ax[1][0], pad=0.02)
+
+im11 = ax[1][1].imshow(n_psficon_m2b, origin='lower')
+ax[1][1].text(5, 70, 'NGC PSF I-band M2b\nnoamp', color='w')
+cbar11 = fig.colorbar(im11, ax=ax[1][1], pad=0.02)
+
+im12 = ax[1][2].imshow(dat_psfH, origin='lower')
+ax[1][2].text(5, 70, 'PSF H-band', color='w')
+cbar12 = fig.colorbar(im12, ax=ax[1][2], pad=0.02)
+plt.show()
+print(oop)
+# '''  #
+
+fig, ax = plt.subplots(2, 4, figsize=(20,8))
+fig.subplots_adjust(wspace=0.1, hspace=0.1)
+im0 = ax[0][0].imshow((n_psficon_m1 - n_psficon_m2a)/n_psficon_m2a, origin='lower', vmin=-1., vmax=1.)
+ax[0][0].text(5, 70, 'NGC (1 - 2a)/2a', color='m')
+cbar0 = fig.colorbar(im0, ax=ax[0][0], pad=0.02)
+im01 = ax[0][1].imshow((p_psficon_m1 - p_psficon_m2a)/p_psficon_m2a, origin='lower', vmin=-1., vmax=1.)
+ax[0][1].text(5, 70, 'PGC (1 - 2a)/2a', color='m')
+cbar01 = fig.colorbar(im01, ax=ax[0][1], pad=0.02)
+im02 = ax[0][2].imshow((n_psficon_m1 - n_psficon_m2b)/n_psficon_m2b, origin='lower', vmin=-1., vmax=1.)
+ax[0][2].text(5, 70, 'NGC (1 - 2b)/2b', color='m')
+cbar02 = fig.colorbar(im02, ax=ax[0][2], pad=0.02)
+im03 = ax[0][3].imshow((p_psficon_m1 - p_psficon_m2b)/p_psficon_m2b, origin='lower', vmin=-1., vmax=1.)
+ax[0][3].text(5, 70, 'PGC (1 - 2b)/2b', color='m')
+cbar03 = fig.colorbar(im03, ax=ax[0][3], pad=0.02)
+
+im1 = ax[1][0].imshow((n_psficon_m2a - n_psficon_m2b)/n_psficon_m2b, origin='lower', vmin=-1., vmax=1.)
+ax[1][0].text(5, 70, 'NGC (2a - 2b)/2b', color='m')
+cbar1 = fig.colorbar(im1, ax=ax[1][0], pad=0.02)
+im11 = ax[1][1].imshow((p_psficon_m2a - p_psficon_m2b)/p_psficon_m2b, origin='lower', vmin=-1., vmax=1.)
+ax[1][1].text(5, 70, 'PGC (2a - 2b)/2b', color='m')
+cbar11 = fig.colorbar(im11, ax=ax[1][1], pad=0.02)
+im12 = ax[1][2].imshow((n_psficon_m2a - n_psficon_m2bnoamp)/n_psficon_m2bnoamp, origin='lower', vmin=-1., vmax=1.)
+ax[1][2].text(5, 70, 'NGC (2a - 2b_noamp)/2b_noamp', color='m')
+cbar12 = fig.colorbar(im12, ax=ax[1][2], pad=0.02)
+im13 = ax[1][3].imshow((p_psficon_m2a - p_psficon_m2bnoamp)/p_psficon_m2bnoamp, origin='lower', vmin=-1., vmax=1.)
+ax[1][3].text(5, 70, 'PGC (2a - 2b_noamp)/2b_noamp', color='m')
+cbar13 = fig.colorbar(im13, ax=ax[1][3], pad=0.02)
+plt.show()
+print(oops)
 
 g2di = do_2dgaussfit(dat_npsfi)  # [1.10704459e-03 4.01447195e+01 4.01799546e+01 8.18216509e-01 8.47028033e-01] # n384
 g2dh = do_2dgaussfit(dat_psfH)  # [ 0.04847133 40.03064685 39.91339118  1.45843277  1.49161195]  # PSF H
